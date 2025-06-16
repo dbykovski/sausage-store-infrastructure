b54644d628ad442a0a806bd33a783f245376021a
std-ext-019-01 std-ext-019-01 <std-ext-019-01@praktikum-services.ru>
2025-06-16 21:21:28 +0000

    Update 2 files
    - /terraform/versions.tf
    - /terraform/provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-    }
-  }
-
-  # Описание бэкенда хранения состояния
-  backend "s3" {
-    endpoint = "https://storage.yandexcloud.net"
-    bucket     = "terraform-state-std-ext-019-01"
-    region     = "ru-central1"
-    key        = "terraform.tfstate"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
-}
-
 provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,4 +7,15 @@ terraform {
             version = ">= 0.87.0"
         }
     }
+
+     # Описание бэкенда хранения состояния
+    backend "s3" {
+        endpoint = "https://storage.yandexcloud.net"
+        bucket     = "terraform-state-std-ext-019-01"
+        region     = "ru-central1"
+        key        = "terraform.tfstate"
+
+        skip_region_validation      = true
+        skip_credentials_validation = true
+    }
 }
