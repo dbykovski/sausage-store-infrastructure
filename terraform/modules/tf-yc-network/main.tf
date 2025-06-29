adaf5ee63e1260ce628f4dd869589ff595c51809
std-ext-019-01 <std-ext-019-01>
2025-06-29 21:12:34 +0300

    update-config

--- terraform/modules/tf-yc-network/main.tf
+++ terraform/modules/tf-yc-network/main.tf
@@ -2,7 +2,7 @@ data "yandex_vpc_network" "default" {
   name = "default"
 }
 
-data "yandex_vpc_subnet" "default" {
+data "yandex_vpc_subnets" "default" {
   for_each = toset(["ru-central1-a", "ru-central1-b"])
 
   name = "${data.yandex_vpc_network.default.name}-${each.key}"
